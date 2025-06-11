b0c60fa931736a1261e251b201ce4bd972f0fb82
Edgardo Muñoz <edgardo.emm20@gmail.com>
2025-06-10 23:41:35 -0500

    second migrate

---
+++ prisma/migrations/20250611044101_rename_columns/migration.sql
@@ -0,0 +1,19 @@
+-- AlterTable
+ALTER TABLE "Course" ADD COLUMN     "studentId" TEXT;
+
+-- CreateTable
+CREATE TABLE "_CourseToStudent" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_CourseToStudent_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE INDEX "_CourseToStudent_B_index" ON "_CourseToStudent"("B");
+
+-- AddForeignKey
+ALTER TABLE "_CourseToStudent" ADD CONSTRAINT "_CourseToStudent_A_fkey" FOREIGN KEY ("A") REFERENCES "Course"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CourseToStudent" ADD CONSTRAINT "_CourseToStudent_B_fkey" FOREIGN KEY ("B") REFERENCES "Student"("id") ON DELETE CASCADE ON UPDATE CASCADE;
